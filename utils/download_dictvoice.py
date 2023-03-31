89248945c040b14d891bf3debabd9dafd80dfcfb
surface@pro.3 <348699103@qq.com>
2023-03-31 13:01:00 +0800

    update dictvoice tool

--- utils/download_dictvoice.py
+++ utils/download_dictvoice.py
@@ -1,10 +1,11 @@
 import requests
 
-word = input("输入单词：").strip()
 
-r = requests.get(f"http://dict.youdao.com/dictvoice?type=1&audio={word}")
-with open(f"{word}_uk.mp3", 'wb') as f:
-    f.write(r.content)
-r = requests.get(f"http://dict.youdao.com/dictvoice?type=0&audio={word}")
-with open(f"{word}_us.mp3", 'wb') as f:
-    f.write(r.content)
+while True:
+    word = input("输入单词：").strip()
+    # r = requests.get(f"http://dict.youdao.com/dictvoice?type=1&audio={word}")
+    # with open(f"{word}_uk.mp3", 'wb') as f:
+    #     f.write(r.content)
+    r = requests.get(f"http://dict.youdao.com/dictvoice?type=0&audio={word}")
+    with open(f"{word}_us.mp3", 'wb') as f:
+        f.write(r.content)
