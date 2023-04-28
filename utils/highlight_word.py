08b2c52bd8823678ca394a5ea1629c19a7e3ffe0
surface@pro.3 <348699103@qq.com>
2023-04-28 09:22:49 +0800

    refactor: Refactor word highlighting for better readability and UX

--- utils/highlight_word.py
+++ utils/highlight_word.py
@@ -2,8 +2,8 @@ import re
 
 
 def highlight_word(word, explanation):
-    explanation = re.sub(r"<span .*?<\/span>", "", explanation)
-    if word in explanation:
+    essence = re.sub(r"<span .*?<\/span>", "", explanation)
+    if word in essence:
         print(f"highlight word: {word}")
         highlighted_word = f'<span style="color: rgb(219, 147, 23);">{word}</span>'
         explanation = explanation.replace(word, highlighted_word)
