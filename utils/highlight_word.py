0a53ea06ad566c6d064ffc2ff0c612d38fb910eb
surface@pro.3 <348699103@qq.com>
2023-04-28 09:27:41 +0800

    refactor: Refactor word highlighting function for reuse

--- utils/highlight_word.py
+++ utils/highlight_word.py
@@ -1,10 +1,17 @@
 import re
 
 
-def highlight_word(word, explanation):
+def highlight_word_impl(word, explanation):
     essence = re.sub(r"<span .*?<\/span>", "", explanation)
     if word in essence:
         print(f"highlight word: {word}")
         highlighted_word = f'<span style="color: rgb(219, 147, 23);">{word}</span>'
         explanation = explanation.replace(word, highlighted_word)
+    return explanation
+
+
+def highlight_word(word, explanation):
+    explanation = highlight_word_impl(word, explanation)
+    if word.endswith('e'):
+        explanation = highlight_word_impl(word[:-1], explanation)
     return explanation
