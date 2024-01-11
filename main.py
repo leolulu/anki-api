fb94c578b91d60733c207761220929bee3241ec3
leolulu-win11 <348699103@qq.com>
2024-01-11 16:35:37 +0800

    新增功能给anki add整合下载语音

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from api.anki_api import Anki
+from utils.download_dictvoice import download_us_voice
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    download_voice_prefix = ":"
     ak = Anki(port=18765)
     while True:
-        ak.add_card(input("输入单词：").strip().lower())
+        input_info = input(f'输入单词(输入"{download_voice_prefix}"开头则只下载发音文件): ').strip().lower()
+        if input_info.startswith(download_voice_prefix):
+            voice_to_download = input_info[len(download_voice_prefix) :]
+            print(f'\n下载"{voice_to_download}"的发音文件...\n')
+            download_us_voice(voice_to_download)
+        else:
+            ak.add_card(input_info)
--- utils/download_dictvoice.py
+++ utils/download_dictvoice.py
@@ -3,11 +3,20 @@ from pathlib import Path
 import requests
 
 
-while True:
-    word = input("输入单词：").strip()
-    # r = requests.get(f"http://dict.youdao.com/dictvoice?type=1&audio={word}")
-    # with open(f"{word}_uk.mp3", 'wb') as f:
-    #     f.write(r.content)
+# def download_uk_voice():
+#     r = requests.get(f"http://dict.youdao.com/dictvoice?type=1&audio={word}")
+#     with open(f"{word}_uk.mp3", "wb") as f:
+#         f.write(r.content)
+
+
+def download_us_voice(word):
+    word = word.strip().lower()
     r = requests.get(f"http://dict.youdao.com/dictvoice?type=0&audio={word}")
-    with open(os.path.join(str(Path.home() / "Downloads"), f"{word}_us.mp3"), 'wb') as f:
+    with open(os.path.join(str(Path.home() / "Downloads"), f"{word}_us.mp3"), "wb") as f:
         f.write(r.content)
+
+
+if __name__ == "__main__":
+    while True:
+        word = input("输入单词：").strip()
+        download_us_voice(word)
